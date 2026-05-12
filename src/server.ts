0b8fbf56e25e814f53d85436d8d4b3000d64a7c9
AmeerSheikh02 <ameersheikh010203@gmail.com>
2026-05-12 19:54:21 +0530

    Setup environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import app from './app';
 
-const PORT = 5000;
+import dotenv from 'dotenv'
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , () => {
     console.log(`Server is running on ${PORT}`);
